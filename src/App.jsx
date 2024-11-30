06cb365bf113146ad2a79c7df3a04ed2966a5cfd
Ahmad Ali <xevenahmad@gmail.com>
2024-11-30 19:45:08 +0500

    mapping photos data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Movies from './components/Movies';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const Movies = () => {
   const [filteredMovies, setFilteredMovies] = useState([]);
@@ -6,14 +7,18 @@ const Movies = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/photos'
+        );
         const data = await response.json();
 
         // Set of albumIds to filter
         const albumIdsToFilter = new Set([1]); // Add the album IDs you want
 
         // Filter items using the Set for quick lookups
-        const filterData = data.filter((item) => albumIdsToFilter.has(item.albumId));
+        const filterData = data.filter((item) =>
+          albumIdsToFilter.has(item.albumId)
+        );
 
         console.log('Filtered Data:', filterData);
         setFilteredMovies(filterData);
@@ -28,14 +33,24 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies</h1>
-      <ul>
-        {filteredMovies.map((movie) => (
-          <li key={movie.id}>
-            <h3>{movie.title}</h3>
-            <img src={movie.thumbnailUrl} alt={movie.title} />
-          </li>
-        ))}
-      </ul>
+      <Container>
+        <Row>
+          {filteredMovies.map((movie) => (
+            <Col lg={4} md={6} sm={12} key={movie.id} className="mb-4">
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={movie.thumbnailUrl}
+                  alt={movie.title}
+                />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
